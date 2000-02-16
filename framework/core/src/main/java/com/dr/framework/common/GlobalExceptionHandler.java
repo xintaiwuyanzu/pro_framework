8773993917fa53879688fa388942492fd4e2f8e5
haolsh <haols@dr.com>
2000-02-16 19:34:48 +0000

    1、去掉spring mobile依赖 2、修改代码生成添加主键空判断

--- framework/core/src/main/java/com/dr/framework/common/GlobalExceptionHandler.java
+++ framework/core/src/main/java/com/dr/framework/common/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.dr.framework.common;
 import com.dr.framework.common.entity.ResultEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mobile.device.Device;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,11 +18,41 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public ResultEntity handleEmployeeNotFoundException(HttpServletRequest request, Device device, Exception ex) {
+    public ResultEntity handleEmployeeNotFoundException(HttpServletRequest request, Exception ex) {
         logger.warn("服务器错误", ex);
-        if (device != null && device.isMobile()) {
+        if (JudgeIsMoblie(request)) {
             return ResultEntity.error("服务器链接超时。");
         }
         return ResultEntity.error("服务器错误：" + ex.getMessage());
+    }
+
+    //判断是否为手机浏览器
+    public boolean JudgeIsMoblie(HttpServletRequest request) {
+        boolean isMoblie = false;
+        String[] mobileAgents = {"iphone", "android", "ipad", "phone", "mobile", "wap", "netfront", "java", "opera mobi",
+                "opera mini", "ucweb", "windows ce", "symbian", "series", "webos", "sony", "blackberry", "dopod",
+                "nokia", "samsung", "palmsource", "xda", "pieplus", "meizu", "midp", "cldc", "motorola", "foma",
+                "docomo", "up.browser", "up.link", "blazer", "helio", "hosin", "huawei", "novarra", "coolpad", "webos",
+                "techfaith", "palmsource", "alcatel", "amoi", "ktouch", "nexian", "ericsson", "philips", "sagem",
+                "wellcom", "bunjalloo", "maui", "smartphone", "iemobile", "spice", "bird", "zte-", "longcos",
+                "pantech", "gionee", "portalmmm", "jig browser", "hiptop", "benq", "haier", "^lct", "320x320",
+                "240x320", "176x220", "w3c ", "acs-", "alav", "alca", "amoi", "audi", "avan", "benq", "bird", "blac",
+                "blaz", "brew", "cell", "cldc", "cmd-", "dang", "doco", "eric", "hipt", "inno", "ipaq", "java", "jigs",
+                "kddi", "keji", "leno", "lg-c", "lg-d", "lg-g", "lge-", "maui", "maxo", "midp", "mits", "mmef", "mobi",
+                "mot-", "moto", "mwbp", "nec-", "newt", "noki", "oper", "palm", "pana", "pant", "phil", "play", "port",
+                "prox", "qwap", "sage", "sams", "sany", "sch-", "sec-", "send", "seri", "sgh-", "shar", "sie-", "siem",
+                "smal", "smar", "sony", "sph-", "symb", "t-mo", "teli", "tim-", "tosh", "tsm-", "upg1", "upsi", "vk-v",
+                "voda", "wap-", "wapa", "wapi", "wapp", "wapr", "webc", "winw", "winw", "xda", "xda-",
+                "Googlebot-Mobile"};
+        if (request.getHeader("User-Agent") != null) {
+            String agent = request.getHeader("User-Agent");
+            for (String mobileAgent : mobileAgents) {
+                if (agent.toLowerCase().indexOf(mobileAgent) >= 0 && agent.toLowerCase().indexOf("windows nt") <= 0 && agent.toLowerCase().indexOf("macintosh") <= 0) {
+                    isMoblie = true;
+                    break;
+                }
+            }
+        }
+        return isMoblie;
     }
 }
