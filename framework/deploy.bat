e633939d9832bef16199726c100f83b388363b9d
haolsh <haols@dr.com>
2018-10-28 10:01:58 +0000

    添加sqlserver兼容处理

--- framework/deploy.bat
+++ framework/deploy.bat
@@ -1,11 +1,11 @@
 cd ./parent
-call mvn clean install -Dmaven.test.skip=true
+call mvn clean deploy -Dmaven.test.skip=true
 cd ../core
-call mvn clean install -Dmaven.test.skip=true
+call mvn clean deploy -Dmaven.test.skip=true
 cd ../codegen
-call mvn clean install -Dmaven.test.skip=true
+call mvn clean deploy -Dmaven.test.skip=true
 cd ../orm
-call mvn clean install -Dmaven.test.skip=true
+call mvn clean deploy -Dmaven.test.skip=true
 cd ../framework
-call mvn clean install -Dmaven.test.skip=true
+call mvn clean deploy -Dmaven.test.skip=true
 cd ..
