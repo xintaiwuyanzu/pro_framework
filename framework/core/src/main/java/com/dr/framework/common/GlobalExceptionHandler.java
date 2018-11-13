37030a7e41f0975060028690ce9e7e1a0cc1bdbb
haolsh <haols@dr.com>
2018-11-13 06:05:21 +0000

    添加spring-mobile相关配置，TODO,这里的mobile还不稳定，还需要修改

--- framework/core/src/main/java/com/dr/framework/common/GlobalExceptionHandler.java
+++ framework/core/src/main/java/com/dr/framework/common/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.dr.framework.common;
 import com.dr.framework.common.entity.ResultEntity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.mobile.device.Device;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,8 +19,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     @ResponseBody
-    public ResultEntity handleEmployeeNotFoundException(HttpServletRequest request, Exception ex) {
+    public ResultEntity handleEmployeeNotFoundException(HttpServletRequest request, Device device, Exception ex) {
         logger.warn("服务器错误", ex);
+        if (device != null && device.isMobile()) {
+            return ResultEntity.error("服务器链接超时。");
+        }
         return ResultEntity.error("服务器错误：" + ex.getMessage());
     }
 }
