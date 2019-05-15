4271abc1e9ec6fe0a4d6e596b0c2be8a71256905
lich <lich@dr.com>
2019-05-15 02:32:45 +0000

    修改管理员查询条件

--- framework/framework/src/main/java/com/dr/framework/sys/controller/PersonController.java
+++ framework/framework/src/main/java/com/dr/framework/sys/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.dr.framework.common.controller.BaseController;
 import com.dr.framework.core.orm.sql.support.SqlQuery;
 import com.dr.framework.sys.entity.Person;
 import com.dr.framework.sys.entity.PersonInfo;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,12 @@ public class PersonController extends BaseController<Person> {
     @Override
     protected void onBeforePageQuery(HttpServletRequest request, SqlQuery<Person> sqlQuery, Person entity) {
         super.onBeforePageQuery(request, sqlQuery, entity);
+        if ( !StringUtils.isEmpty(entity.getUserName())){
+            sqlQuery.equal(PersonInfo.USERNAME,entity.getUserName());
+        }
+        if ( !StringUtils.isEmpty(entity.getUserCode())){
+            sqlQuery.equal(PersonInfo.USERCODE,entity.getUserCode());
+        }
         sqlQuery.orderBy(PersonInfo.ID);
     }
 }
