aebc0943ce72ee4ef1e3b59123ba67bc676d321d
haolsh <haolsh@dr.com>
2020-09-02 09:18:05 +0800

    删除流程相关代码，单独提取成模块

--- framework/activiti/src/main/java/com/dr/process/activiti/spring/boot/autoconfigure/ProcessEngineConfigurationConfigurer.java
+++
@@ -1,7 +0,0 @@
-package com.dr.process.activiti.spring.boot.autoconfigure;
-
-import org.activiti.spring.SpringProcessEngineConfiguration;
-
-public interface ProcessEngineConfigurationConfigurer {
-    void configure(SpringProcessEngineConfiguration processEngineConfiguration);
-}
